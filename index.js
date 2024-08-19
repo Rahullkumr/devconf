9303269c739931ac65873e2dc9ac02522717693c
vaibdix <dixitvaib@gmail.com>
2024-08-19 22:00:12 +0530

    chore: scroll animation and trigger

--- index.js
+++ index.js
@@ -37,3 +37,39 @@ const carouselElements = document.querySelectorAll(".splide");
     window.addEventListener('scroll', handleScroll);
     handleScroll();
   });
+
+
+
+  const lenis = new Lenis();
+  function raf(time) {
+    lenis.raf(time * 0.9);
+    requestAnimationFrame(raf);
+  }
+  requestAnimationFrame(raf);
+  ScrollTrigger.defaults({
+    scroller: document.body,
+  });
+
+  const container = document.querySelector(".container");
+  console.log(container.offsetWidth);
+
+  function getScrollAmount() {
+    let containerWidth = container.scrollWidth;
+    return -(containerWidth - window.innerWidth);
+  }
+
+  const tween = gsap.to(container, {
+    x: getScrollAmount,
+    duration: 3.5,
+    ease: "none",
+  });
+
+  ScrollTrigger.create({
+    trigger: ".container-wrapper",
+    start: "top 0vh",
+    end: () => `+=${getScrollAmount() * -1} center`,
+    pin: true,
+    animation: tween,
+    scrub: 1,
+    invalidateOnRefresh: true,
+  });
